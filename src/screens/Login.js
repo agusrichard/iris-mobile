9f58585d24feec314539a766150724bb2a009e12
agusrichard <agus.richard21@gmail.com>
2020-07-24 13:04:38 +0700

    Gotta prepare i guess

---
+++ src/screens/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, View, Text,  } from 'react-native'
+
+export default function Login() {
+  
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
---
+++ src/screens/Predict.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, View, Text,  } from 'react-native'
+
+export default function Predict() {
+  
+  return (
+    <View>
+      <Text>Predict</Text>
+    </View>
+  )
+}
